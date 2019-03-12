72438d86a6b9d7821aa9ab441282d101c1cf8461
erodxd <emrod718@gmail.com>
2019-03-12 16:36:04 -0400

    working version

--- bin/run.rb
+++ bin/run.rb
@@ -6,3 +6,8 @@ require_relative "../lib/command_line_interface.rb"
 welcome
 character = get_character_from_user
 show_character_movies(character)
+
+
+# puts a wlecome
+# save requested character to a variable
+# show the movies the character is in
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,14 @@
+require 'rest-client'
+require 'json'
+require 'pry'
+
 def welcome
-  # puts out a welcome message here!
+ # puts out a welcome message here!
+ p "Welcome to the Star Wars API Application!"
 end
 
 def get_character_from_user
-  puts "please enter a character name"
-  # use gets to capture the user's input. This method should return that input, downcased.
+ puts "please enter a character name"
+ gets.chomp
+ # use gets to capture the user's input. This method should return that input, downcased.
 end
